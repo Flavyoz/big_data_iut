8958e8c66a1f224db64e4ae8630af7e9056fcf45
Flavyoz <123741254+Flavyoz@users.noreply.github.com>
2025-05-06 10:25:56 +0200

    Add files via upload

---
+++ TagCounts.py
@@ -0,0 +1,22 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+class TagCounts(MRJob):
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_tags,
+                   reducer=self.reducer_count_tags)
+        ]
+
+    def mapper_get_tags(self, _, line):
+        try:
+            (userID, movieID, tag, timestamp) = line.split('\t')
+            yield tag, 1
+        except Exception:
+            pass
+
+    def reducer_count_tags(self, tag, counts):
+        yield tag, sum(counts)
+
+if __name__ == '__main__':
+    TagCounts.run()
